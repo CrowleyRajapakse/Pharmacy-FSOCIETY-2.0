22f248023a28c977c73e8517a3fb4a233989decc
Crowley Rajapakse <srrajapakse1@gmail.com>
2017-05-05 19:43:39 +0530

    Adding dispensing and printing ability to prescriptions

--- modules/prescriptions/client/controllers/prescriptions.client.controller.js
+++ modules/prescriptions/client/controllers/prescriptions.client.controller.js
@@ -17,11 +17,28 @@
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
+    vm.printDispense = printDispense;
 
     // Remove existing Prescription
     function remove() {
       if ($window.confirm('Are you sure you want to delete?')) {
         vm.prescription.$remove($state.go('prescriptions.list'));
+      }
+    }
+
+    //Dispnese and print
+    function printDispense(div) {
+      if ($window.confirm('Are you sure you want to Dispense?')) {
+        var docHead = document.head.outerHTML;
+        var printContents = document.getElementById(div).outerHTML;
+        var winAttr = "location=yes, statusbar=no, menubar=no, titlebar=no, toolbar=no,dependent=no, width=865, height=600, resizable=yes, screenX=200, screenY=200, personalbar=no, scrollbars=yes";
+
+        var newWin = window.open("", "_blank", winAttr);
+        var writeDoc = newWin.document;
+        writeDoc.open();
+        writeDoc.write('<!doctype html><html>' + docHead + '<body onLoad="window.print()">' + printContents + '</body></html>');
+        writeDoc.close();
+        newWin.focus();
       }
     }
 
