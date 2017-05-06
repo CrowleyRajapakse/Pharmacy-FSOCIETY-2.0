44bd3913c863bddeb5e277b7f349d24c04bd785c
isuru jayathilaka <isuru jayathilaka>
2017-05-06 19:52:07 +0530

    isuru

---
+++ modules/drugs/server/config/drugs.server.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Isuru Jayathilaka on 05/06/2017.
+ */
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+var path = require('path'),
+  config = require(path.resolve('./config/config'));
+
+/**
+ * Prescriptions module init function.
+ */
+
+module.exports = function (app, db) {
+
+};
---
+++ modules/drugs/server/controllers/drugs.server.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Isuru Jayathilaka on 05/06/2017.
+ */
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+/*var path = require('path'),
+    mongoose = require('mongoose'),
+    Prescription = mongoose.model('Prescription'),
+    errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+    _ = require('lodash');*/
+
+/**
+ * Stock Schema
+ */
